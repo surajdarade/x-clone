6772fc873afafa87a213822511c278d2dbe6baa0
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-03-28 21:44:16 +0530

    getmyprofile controller added

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -150,6 +150,14 @@ export const bookmark = async (req: Request, res: Response) => {
 
 // GETPROFILE CONTROLLER
 
-export const getMyProfile = (req: Request, res: Response) => {
+export const getMyProfile = async (req: Request, res: Response) => {
+  try {
+    const loggedInUserId = req.params.id;
 
-}
+    const user = await UserModel.findById({ _id: loggedInUserId }).select("-password");
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.log(error);
+  }
+};
