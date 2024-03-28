5f9b95e6ec8c2c8a1fc6f3c0c13d76aff2781df3
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-03-28 22:58:05 +0530

    allTweets and getFollowingTweets controllers and routes added

--- server/controllers/tweetController.ts
+++ server/controllers/tweetController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { TweetModel } from "../models/tweetModel";
+import { UserModel } from "../models/userModel";
 
 export const createTweet = async (req: Request, res: Response) => {
   try {
@@ -41,7 +42,7 @@ export const likeOrDislike = async (req: Request, res: Response) => {
     const loggedInUserId = req.body.id;
     const tweetId = req.params.id;
 
-    const tweet = await TweetModel.findOne({ _id: tweetId }); 
+    const tweet = await TweetModel.findOne({ _id: tweetId });
 
     if (!tweet) {
       return res.status(404).json({ message: "Tweet not found!" });
@@ -59,6 +60,61 @@ export const likeOrDislike = async (req: Request, res: Response) => {
       });
       return res.status(200).json({ message: "Tweet Liked!" });
     }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// GET ALL TWEETS CONTROLLER
+
+export const getAllTweets = async (req: Request, res: Response) => {
+  // Logged in + Following user tweets
+
+  try {
+    const id = req.params.id;
+
+    const loggedInUser = await UserModel.findById(id);
+
+    if (!loggedInUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const loggedInUserTweets = await TweetModel.find({ userId: loggedInUser });
+
+    const followingUserTweets = await Promise.all(
+      loggedInUser.following.map((otherUserId) => {
+        return TweetModel.find({ userId: otherUserId });
+      })
+    );
+
+    const allTweets = loggedInUserTweets.concat(...followingUserTweets);
+
+    return res.status(200).json({ tweets: allTweets });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// GET FOLLOWING TWEETS ONLY
+
+export const getFollowingTweets = async (req: Request, res: Response) => {
+
+  try {
+    const id = req.params.id;
+
+    const loggedInUser = await UserModel.findById(id);
+
+    if (!loggedInUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const followingUserTweets = await Promise.all(
+      loggedInUser.following.map((otherUserId) => {
+        return TweetModel.find({ userId: otherUserId });
+      })
+    );
+
+    return res.status(200).json({ tweets: followingUserTweets });
   } catch (error) {
     console.log(error);
   }
--- server/routes/tweetRoutes.ts
+++ server/routes/tweetRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { createTweet, deleteTweet, likeOrDislike } from "../controllers/tweetController";
+import { createTweet, deleteTweet, getAllTweets, getFollowingTweets, likeOrDislike } from "../controllers/tweetController";
 
 const router = express.Router();
 
 router.post("/create", createTweet);
 router.delete("/delete/:id", deleteTweet);
 router.put("/like/:id", likeOrDislike);
+router.get("/allTweets/:id", getAllTweets);
+router.get("/followingTweets/:id", getFollowingTweets);
 
 export default router;
