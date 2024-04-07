b51a526a4ed251e077a5e95a383f153c65abdf3f
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-04-07 09:00:12 +0530

    reach to top always on route change

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { UserState } from "../store/userSlice";
 import { Toaster } from "react-hot-toast";
@@ -11,13 +11,18 @@ import { Outlet } from "react-router-dom";
 const Home = () => {
   const { user } = useSelector((store: { user: UserState }) => store.user);
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!user) {
       navigate("/signin");
     }
 
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
 
     const handleScrollToTop = () => {
       window.scrollTo(0, 0);
@@ -28,7 +33,7 @@ const Home = () => {
     return () => {
       window.removeEventListener("beforeunload", handleScrollToTop);
     };
-  }, [user, navigate]);
+  }, [user, navigate, pathname]);
 
   const _id = user ? user._id : "";
 
