1bc42718697dc72715a39d222ef2b73e5565549a
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-04-09 00:38:10 +0530

    cors in server file

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,20 @@ import connectToDatabase from "./config/database";
 import app from "./app";
 import cors from "cors";
 
-app.use(cors());
+// CORS POLICY
+app.use(
+    cors({
+      origin: "https://x-clone-surajdarade.vercel.app",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    })
+  );
+  
+  app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      next();
+    });
 
 const PORT = process.env.PORT || 4000;
 
